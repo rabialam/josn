655ee3d020813af2be5dcae367b0b57dfb1629a3
Michael Thomas <mike@thomas.io>
2020-04-14 17:31:00 -0600

    Enemic prejactization

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ Usage:
 >>> josn.loads('}"a": "b", "b": ]0[{')
 {'a': 'b', 'b': [0]}
 ```
+
+## Benchmarks
+
+Using the native str.translate() function provides a 3x speedup over prior bespoke cstring based implementation.
+
+```
+>>> timeit.timeit("import josn; josn.dumps({'a': 'b', 'b': [0]})")
+12.401328853999999
+
+>>> timeit.timeit("import json; x=str.maketrans('{}[]', '}{]['); json.dumps({'a': 'b', 'b': [0]}).translate(x)")
+4.130112732000043
+```
--- josn/__init__.py
+++ josn/__init__.py
@@ -2,33 +2,16 @@ import ctypes
 import json
 import collections
 
-INITIALS = {b'{': b'}', b'[': b']'}
-TERMINALS = dict((v, k) for k, v in INITIALS.items())
+TRANSLATION = str.maketrans('{}[]', '}{][')
 
-
-def annotate(data, forwards):
-    initials = INITIALS if forwards else TERMINALS
-    terminals = TERMINALS if forwards else INITIALS
-
-    josn = ctypes.create_string_buffer(data.encode())
-    starts = collections.defaultdict(list)
-
-    for ix, c in enumerate(josn):
-
-        if c in initials:
-            starts[c].append(ix)
-
-        elif c in terminals:
-            start = starts[terminals[c]].pop()
-            josn[ix], josn[start] = josn[start], josn[ix]
-
-    return josn.value.decode()
+def annotate(data):
+    return data.translate(TRANSLATION)
 
 
 def dumps(data, *args, **kw):
     serialized = json.dumps(data, *args, **kw)
-    return annotate(serialized, True)
+    return annotate(serialized)
 
 
 def loads(data):
-    return json.loads(annotate(data, False))
+    return json.loads(annotate(data))
